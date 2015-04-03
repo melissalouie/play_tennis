b7859934ae4e3acaf566a020c9f22f3732270111
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-03 13:31:59 -0600

    add nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/signin' => 'sessions#new', as: 'signin'
   post '/signin' => 'sessions#create'
+  get '/signup' => 'users#new', as: 'signup'
+  get '/signout' => 'sessions#destroy', as: 'signout'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
