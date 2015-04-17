5c5c892bc5f71c1c211ca0f24c3947700203b2d8
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-17 10:28:43 -0600

    add footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post '/signin' => 'sessions#create'
   get '/signup' => 'users#new', as: 'signup'
   get '/signout' => 'sessions#destroy', as: 'signout'
+  get '/about' => 'welcome#about'
+  get '/contact' => 'contact#about'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
