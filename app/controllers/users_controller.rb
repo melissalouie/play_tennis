902fd41274c7454ad2841aa8f20571b11efc3f39
melissalouie <mwq.louie@gmail.com>
2015-06-08 15:46:09 -0600

    add controller logic to find player matches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,8 @@ class UsersController < ApplicationController
 
   def match
     if current_user
-      @user = current_user
-      @matches_10miles = User.near([ current_user.latitude, current_user.longitude], 10).where.not(id: current_user.id)
-
+      @matches_10miles = User.near([current_user.latitude, current_user.longitude], 10).where.not(id: current_user.id)
+      @matches_20miles = User.near([current_user.latitude, current_user.longitude], 20).where.not(id: current_user.id)
     else
       redirect_to root_path, notice: 'You must be logged in to view this page.'
     end
